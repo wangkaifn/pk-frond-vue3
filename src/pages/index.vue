c3b2d6abb3450df9e45715e76f124a30c525755c
akai <kai150036@163.com>
2024-05-22 19:02:21 +0800

    feat: 自动导入Autolmport+vueUse

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div ref="target" class="w-40 h-40 bg-gray-300">
+      X: {{ x }} Y: {{ y }} isOutside: {{ isOutside }}
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const target = ref(null)
+
+const { x, y, isOutside } = useMouseInElement(target)
+</script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,37 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 // uno css
 import UnoCSS from 'unocss/vite'
+
+import AutoImport from 'unplugin-auto-import/vite'
+
+// 如果使用了AutoImport vue-router 需要从 VueRouterAutoImports引入
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     VueRouter({
       /* options */
     }),
+
     vue(),
     vueJsx(),
-    UnoCSS()
+    UnoCSS(),
+    AutoImport({
+      /* options */
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+        '@vueuse/core'
+      ]
+    })
   ],
   resolve: {
     alias: {

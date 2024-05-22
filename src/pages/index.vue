7152f0381135bb63fdf2a283f25f0dd338e24fe1
akai <kai150036@163.com>
2024-05-22 21:50:30 +0800

    feat: 图标方案:iconify-sets

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,16 @@
       X: {{ x }} Y: {{ y }} isOutside: {{ isOutside }}
     </div>
     <el-button>Button</el-button>
+    <!-- A basic anchor icon from Phosphor icons -->
+    <div class="i-ph:airplane-fill w-12 h-12"></div>
+    <!-- An orange alarm from Material Design Icons -->
+    <div class="i-mdi-alarm text-orange-400" />
+    <!-- A large Vue logo -->
+    <div class="i-logos-vue text-3xl" />
+    <!-- Sun in light mode, Moon in dark mode, from Carbon -->
+    <button class="i-carbon-sun dark:i-carbon-moon" />
+    <!-- Twemoji of laugh, turns to tear on hovering -->
+    <div class="i-twemoji-grinning-face-with-smiling-eyes hover:i-twemoji-face-with-tears-of-joy" />
   </div>
 </template>
 
